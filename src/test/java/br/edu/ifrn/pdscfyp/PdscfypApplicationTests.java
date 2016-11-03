802712c746abddee2c4eaeec2cd66609d72f28be
Mateus Oliveira <mateusocb@gmail.com>
2016-11-03 02:47:18 -0300

    commit inicial

---
+++ src/test/java/br/edu/ifrn/pdscfyp/PdscfypApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.ifrn.pdscfyp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdscfypApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
