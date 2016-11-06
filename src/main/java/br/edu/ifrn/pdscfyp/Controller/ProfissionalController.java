72726d442715a3b3b43beed52e72e8c6538cd7d2
Mateus Oliveira <mateusocb@gmail.com>
2016-11-06 15:56:19 -0300

    arrumando o cadastrar profissional2

--- src/main/java/br/edu/ifrn/pdscfyp/Controller/ProfissionalController.java
+++ src/main/java/br/edu/ifrn/pdscfyp/Controller/ProfissionalController.java
@@ -22,48 +22,47 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class ProfissionalController {
-    
+
     @RequestMapping("/profissionais")
     public String ListProfissionais(Model model) {
-        
+
         Set<Profissional> profissionais = Profissional.getProfissionais();
-        
+
         model.addAttribute("profissionais", profissionais);
-     
+
         return "listProfissionais";
     }
-    
+
     @RequestMapping("/profissional/{username}")
     public String getProfissional(@PathVariable("username") String username, Model model) {
         Profissional p = Profissional.getProfissionalByLogin(username);
-        
+
         model.addAttribute("profissional", p);
-        
+
         return "showProfissional";
     }
-    
+
     @RequestMapping("/ranking")
     public String RankingProfissionais(Model model) {
-  
+
         return "rankingProfissionais";
     }
-    
+
     @RequestMapping("/cadastrar")
     public String CadastrarProfissional() {
-        
+
         return "cadastrarProfissional";
     }
-    
-    @RequestMapping(value="/cadastrar", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
     public String CadastroProfissional(@RequestParam("nome") String nome,
-            @RequestParam("email") String email,@RequestParam("login") String login,
-            @RequestParam("senha") String senha, @RequestParam("idnacional") String idNacional,
-            @RequestParam("descricao") String descricao, @RequestParam("profissao") String profisao){
-        
+            @RequestParam("email") String email, @RequestParam("login") String login,
+            @RequestParam("senha") String senha, @RequestParam("descricao") String descricao, 
+            @RequestParam("profissao") String profisao) {
+
         Profissional p = new Profissional(nome, email, login, senha, 0, descricao, profisao, 0, 0, 0);
-        
         Profissional.addProfissional(p);
-        
+
         return "cadastrarProfissional";
     }
 }
