676dd55be3162286b79c6304b2e4f8c2a37792ed
Mateus Oliveira <mateusocb@gmail.com>
2016-11-06 17:32:43 -0300

    arrumando o cadastrar profissional7

--- src/main/java/br/edu/ifrn/pdscfyp/Controller/ProfissionalController.java
+++ src/main/java/br/edu/ifrn/pdscfyp/Controller/ProfissionalController.java
@@ -54,7 +54,7 @@ public class ProfissionalController {
         return "cadastrarProfissional";
     }
 
-    @RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+    @RequestMapping(value = "/cadastro", method = RequestMethod.POST)
     public String CadastroProfissional(@RequestParam("nome") String nome,
             @RequestParam("email") String email, @RequestParam("login") String login,
             @RequestParam("senha") String senha, @RequestParam("descricao") String descricao, 
